5daf3a7e5b82eda2f86f163a1f73966fa3f08b42
nicgibsonUNI <142642085+nicgibsonUNI@users.noreply.github.com>
2024-04-21 23:03:13 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string firstName = "John";
+    std::string lastName = "Smith";
+    std::string streetAddress = "123 Main St";
+    std::string city = "Denver";
+    std::string zipCode = "80207";
+
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
