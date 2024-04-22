aa684954de9b07f8f8719c6b9b33bddfd323af54
nicgibsonUNI <142642085+nicgibsonUNI@users.noreply.github.com>
2024-04-21 23:50:15 -0600

    Add files via upload

---
+++ corrected_CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,26 @@
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.*/
+
+
+#include <iostream>
+#include <conio.h>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+   //Standard Ouput Statement
+   cout <<"Welcome to this C++ Program" <<endl;
+
+   cout <<"I have corrected all errors for this program." <<endl;
+
+   // Wait For Output Screen
+   cout << "Press enter to continue.";
+   cin.get(); // Waits for user input
+
+   //Main Function return Statement
+   return 0;
+}
+
